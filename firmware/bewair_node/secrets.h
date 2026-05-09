3f8f0bb652370872e65b241350971f8e2457a68a
ChrstphrAngelo <roque.christopher.angelo@gmail.com>
2026-05-09 16:47:00 +0800

    Hide HiveMQ credentials from version control

--- firmware/bewair_node/secrets.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// HiveMQ Cloud credentials.
-// Do NOT commit this file to a public repo. Add `firmware/bewair_node/secrets.h` to .gitignore.
-#define MQTT_USERNAME "vortex5-app"
-#define MQTT_PASSWORD "K7mN2pQ9vR4xT6yW"
